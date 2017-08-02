666db2ef5e514f00e35a70cd238e92a54428b70b
Myles Parker <myles.parker@NUVI0018.local>
2017-08-01 21:44:46 -0600

    Added functional mobile menu bar

--- javascript/app.js
+++ javascript/app.js
@@ -4,6 +4,28 @@
 
 $(".fade").hide(0).delay(500).fadeIn(3000);
 
-$(".fa-bars").click(function(){$("#mobileMenu").slideToggle()})
+$("#menu-tag").click(function(){$("#mobile-nav").slideToggle()});
 
 $('.scrolly').scrolly();
+
+var theWindow = $(window);
+
+setTimeout(function() {
+    checkWidth();
+}, 500);
+
+function checkWidth() {
+    if(theWindow.width() > 900) {
+        $("#mobile-nav").css("display", "none");
+    }
+    setTimeout(function () {
+        checkWidth()
+    }, 500);
+    return true
+}
+
+window.onclick = function(event) {
+    if (!(event.target.matches("#menu-tag") || event.target.matches(".fa-bars") || event.target.matches(".fa-bars:before") || event.target.matches(".mob-navi"))) {
+        $("#mobile-nav").slideUp();
+    }
+};
