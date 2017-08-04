42681e5e167199f4705c895b30624b39262dba0a
Myles Parker <myles.parker@NUVI0018.local>
2017-08-04 11:38:35 -0600

    Filled sections with content

--- javascript/app.js
+++ javascript/app.js
@@ -36,7 +36,6 @@ $(window).scroll(function() {
         hH = $(skills).outerHeight(),
         wH = $(window).height(),
         wS = $(this).scrollTop();
-    console.log((hT-wH) , wS);
     if (wS > (hT+hH-wH)){
         $('#scroll-to').fadeIn(3500);
     }
